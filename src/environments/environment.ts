3d1379ed0fbf57eaeb0e2a841b36fe79452ee313
jose lara <jose.lara.cuadrado@gmail.com>
2025-03-24 22:46:30 +0100

    remove data from environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
-    apiKey: 'AIzaSyC0C4QTFrY5TxxYZNYK8nvw4Z4ITwCXeZc',
-    authDomain: 'recipe-finder-a977f.firebaseapp.com',
-    projectId: 'recipe-finder-a977f',
-    storageBucket: 'recipe-finder-a977f.firebasestorage.app',
-    messagingSenderId: '224043316407',
-    appId: '1:224043316407:web:ee3f85984db114f6e4c932'
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: ''
   }
 }; 
