4df8120b8a217afb2896e13f9020be7edbb463e0
Jose Lara <jlara@hotelbeds.com>
2025-04-07 13:58:19 +0200

    push environment.ts

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+      apiKey:  'default-api-key',
+      authDomain: 'default-auth-domain',
+      projectId: 'default-project-id',
+      storageBucket: 'default-storage-bucket',
+      messagingSenderId: 'default-messaging-sender-id',
+      appId: 'default-app-id'
+    }
+  }; 
