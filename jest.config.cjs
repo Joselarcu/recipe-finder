db2c441bb9f4706350c118a00bb2013577e7d8aa
Jose Lara <jlara@hotelbeds.com>
2025-04-07 22:33:25 +0200

    check tests coverage and then check if the file is generated with the coverage before sonar analyze it

--- jest.config.cjs
+++ jest.config.cjs
@@ -4,5 +4,11 @@ module.exports = {
   setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
   collectCoverage: true,
   coverageDirectory: '<rootDir>/coverage/',
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.spec.ts',
+    '!src/main.ts',
+    '!src/environments/**'
+  ],
   coverageReporters: ['html', 'text', 'lcov', 'text-summary'],
 };
