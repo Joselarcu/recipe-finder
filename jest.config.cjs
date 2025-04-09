9130a4b5da5d7f22276cf7586014fde726c3157d
Jose Lara <jlara@hotelbeds.com>
2025-04-09 21:42:17 +0200

    exclude files for coverage

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,5 +11,5 @@ module.exports = {
     '!src/environments/**'
   ],
   coverageReporters: ['html', 'text', 'lcov', 'text-summary'],
-  coveragePathIgnorePatterns: ['src/main.ts', 'src/environments/**', 'app.routes.server.ts', 'appendFile.config.ts', 'server.ts']
+  coveragePathIgnorePatterns: ['/node_modules/', 'src/main.ts', 'src/environments/', 'src/app/app.routes.server.ts', 'src/app/app.config.ts', 'src/server.ts', 'src/app/app.routes.ts', 'src/main.ts']
 };
