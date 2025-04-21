3d9f57b99e3525dd63b0bdf6a6bf6daa6d395e7d
Jose Lara <jlara@hotelbeds.com>
2025-04-21 16:26:49 +0200

    finish test

--- src/app/components/books/books.component.ts
+++ src/app/components/books/books.component.ts
@@ -1,16 +1,19 @@
 import { httpResource } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Book } from '../../models/book.model';
+import { BookInputComponent } from '../book-input/book-input.component';
 
 @Component({
   selector: 'app-books',
-  imports: [],
+  imports: [BookInputComponent],
   templateUrl: './books.component.html',
   styleUrl: './books.component.scss'
 })
 export class BooksComponent {
 
   apiUrl = 'https://www.freetestapi.com/api/v1/books';
+
+  bookTitle = signal('')
 
   //httpResource should be in a service
   books = httpResource< Book[]>(
